641f653b1968c7632e566f38769c88705773aa27
Yungsorex <164731814+Yungsore@users.noreply.github.com>
2024-06-20 09:47:57 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const DATABASE_URL =
   process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
   VERSION: require('./package.json').version,
-  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  SESSION_ID: (process.env.SESSION_ID || '').trim(20_6_4ee4_c8f2_f836),
   DATABASE:
     DATABASE_URL === databasePath
       ? new Sequelize({
